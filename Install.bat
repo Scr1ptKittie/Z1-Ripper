feec74c382f0bf3daedc34d5617c03239d6243bf
Z3o2 <157680657+Z3o2@users.noreply.github.com>
2024-02-05 17:34:23 -0800

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,10 +1,13 @@
 @echo off
 echo Installing necessary Python packages...
 
+pip install os
+pip install time
+pip install ctypes
 pip install watchdog
 pip install colorama
-pip install keyboard
 pip install plyer
+pip install keyboard
 pip install pywin32
 
 echo Installation complete.
